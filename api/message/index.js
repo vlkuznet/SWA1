a435f66034a86e5d0b2710c93e3b762f357fea6f
vlkuznet <56447814+vlkuznet@users.noreply.github.com>
2022-01-27 14:38:21 -0600

    modified Function a bit

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "This message is from the Function app."
+        text: "This message is from the Function app. Random number to prove we're working: "+ Math.floor(Math.random()*1000)
     });
 };
